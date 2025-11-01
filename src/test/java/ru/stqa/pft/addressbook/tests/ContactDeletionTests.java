8fb47d0f737d1b67ae84df2308593bdd613cb42a
Eva <kbe4989@yandex.ru>
2025-11-02 00:06:19 +0300

    update class

---
+++ src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -0,0 +1,38 @@
+package ru.stqa.pft.addressbook.tests;
+
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ContactDeletionTests extends TestBase { // Перименовали класс на ContactDeletionTests
+
+    @BeforeEach
+    public void prepareContactDeletion() { // Переименовали метод на prepareContactDeletion
+        appManager.goTo().homePage();
+        if (appManager.contact().all().isEmpty()) {
+            appManager.goTo().contactPage();
+            appManager.contact().create(new ContactData()
+                    .withFirstName("TestFirstName").withMiddleName("TestMiddleName")
+                    .withLastName("TestLastName").withNickname("TestNickname")
+                    .withCompany("TestCompany").withAddress("TestAddress")
+                    .withFirstEmail("TestEmail").withGroup("GroupName")
+                    .withHomePhone("123456").withMobilePhone("+79854612312").withWorkPhone("123456789"));
+        }
+    }
+
+    @Test
+    public void testContactDeletion() {
+        Contacts beforeContactList = appManager.contact().all();
+        ContactData deletedContact = beforeContactList.iterator().next();
+
+        appManager.contact().delete(deletedContact);
+        appManager.goTo().homePage();
+        assertThat(appManager.contact().count(), equalTo(beforeContactList.size() - 1));
+        Contacts afterContactList = appManager.contact().all();
+        assertThat(afterContactList, equalTo(beforeContactList.without(deletedContact)));
+    }
+}
